036838acd42b6c20f822921b5ad9ee6bd9baed8b
Rodolfo Lemes <rodolfo_fero@hotmail.com>
2021-02-01 13:45:26 -0300

    Mudança no index.js apenas para testes das classes

--- src/index.js
+++ src/index.js
@@ -1,53 +1,11 @@
-const axios = require('axios')
-const cheerio = require('cheerio')
 const fs = require('fs')
 
-const detailsInsertValue = require('./utils/ProgramaThor/detailsInsertValue')
+const Vulpi = require('./classes/Vulpi')
 
-const BASE_URL = 'https://programathor.com.br/'
+const vulpi = new Vulpi()
 
-let countPage = 1
+//vulpi.generateOptions()
 
-async function scrapeProgramaThor() {
-  let jobs = []
-
-  let done = false
-  while(!done) {
-    const response = await axios.get(BASE_URL + 'jobs/page/' + countPage)
-  
-    const $ = cheerio.load(response.data)
-    
-    $('div.cell-list:not(.min-height-180)').each((i, op) => {
-      const url = $(op).children().attr('href')
-      let name = $(op).find('h3').text()
-      if(name.match(/^VENCIDA/)) {
-        done = true
-        return
-      }
-      name = name.replace(/NOVA$/, '')
-      let details = []
-      $(op).find('div.cell-list-content-icon > span').each((index, element) => {
-        details.push($(element).text())
-      })
-      let tags = []
-      $(op).find('span.tag-list.background-gray').each((index, element) => {
-        tags.push($(element).text())
-      })
-      jobs.push({
-        url: BASE_URL + url,
-        name,
-        details: detailsInsertValue(details),
-        tags
-      })
-    })
-    console.log(`Página ${countPage} concluída`)
-    countPage ++
-    if(countPage === 10) break
-  }
-  return jobs
-}
-
-scrapeProgramaThor().then((jobs) => {
-  fs.writeFileSync('jobsProgramathor.json', JSON.stringify(jobs))
-  console.log('Done!')
+vulpi.scrape().then((jobs) => {
+  console.log(jobs)
 })
